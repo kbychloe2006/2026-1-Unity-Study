b3d12a55e2cd41f0754617513dcbf031d81351a5
kbychloe2006 <kbychloe2006@naver.com>
2026-04-27 23:41:34 +0900

    Feat: 3주차 실습 내용

---
+++ 구보윤/3주차 실습/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    Rigidbody2D rb2d;
+    SurfaceEffector2D SurfaceEffector2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); 
+        SurfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlayer();
+        RespeondToBoost();
+    }
+
+    void RespeondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            SurfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            SurfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+       if(Input.GetKey(KeyCode.LeftArrow))
+       {
+            rb2d.AddTorque(torqueAmount);
+       }
+       else if(Input.GetKey(KeyCode.RightArrow))
+       {
+            rb2d.AddTorque(-torqueAmount);
+       }
+    }
+}
---
+++ 구보윤/3주차 실습/UnityStudy_week3.md
@@ -0,0 +1,22 @@
+### #40 파티클 이펙트 소개
+
+particle system 를 이용하여 파티클 이펙트 만들기
+
+![](https://velog.velcdn.com/images/dong9rami/post/2fec810b-e54e-4400-b2c3-c1241266de83/image.gif)
+
+### #41 파티클 이펙트 트리거
+
+![](https://velog.velcdn.com/images/dong9rami/post/bd907411-f652-4731-acf2-69f9064d75d2/image.png)
+
+![](https://velog.velcdn.com/images/dong9rami/post/1ef71e43-8b63-4828-be47-33ef20b3602e/image.png)
+
+### #42 모먼트 튜닝
+
+![](https://velog.velcdn.com/images/dong9rami/post/ffc28920-4dbe-41ac-bb2b-0155e6860874/image.png)
+
+### #43 Find Object Of Type 사용하기
+
+![](https://velog.velcdn.com/images/dong9rami/post/4b8502ee-6450-4feb-8512-722d31362e38/image.png)
+
+![](https://velog.velcdn.com/images/dong9rami/post/62b9e369-dc2f-45b9-a3bb-ae8b5d340243/image.png)
+
